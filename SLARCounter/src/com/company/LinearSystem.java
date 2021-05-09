c37d461509e596277c9ebddf06c4e97a763c2131
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-05-09 19:25:56 +0300

    Add files via upload

---
+++ SLARCounter/src/com/company/LinearSystem.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import com.company.gauss.Gauss;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinearSystem<N extends Number, T extends Gauss<N, T>> {
+    private List<T> list = new ArrayList<T>();
+
+    public T get(int index){
+        return list.get(index);
+    }
+
+    public void push(T elem){
+        list.add(elem);
+    }
+
+    public int size(){
+        return list.size();
+    }
+
+    public N itemAt(int i, int j){
+        return list.get(i).at(j);
+    }
+}
